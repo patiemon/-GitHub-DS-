f660f6d2c9adfa09966d83306b70bbabe1f9bf78
Patiemon <hj-g19017@sist.ac.jp>
2020-12-04 10:15:18 +0900

    初めてのコミット

---
+++ Dinosaur Shooting/main.cpp
@@ -0,0 +1,19 @@
+//########## ヘッダーファイル読み込み ##########
+#include "DxLib.h"
+
+//########## マクロ定義 ##########
+#define GAME_WIDTH	800	//画面の横の大きさ
+#define GAME_HEIGHT	600	//画面の縦の大きさ
+#define GAME_COLOR	32	//画面のカラービット
+
+#define GAME_WINDOW_BAR	0	//タイトルバーはデフォルトにする
+#define GAME_WINDOW_NAME	"GAME TITLE"	//ウィンドウのタイトル
+
+//########## プログラムで最初に実行される関数 ##########
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(TRUE);			//ウィンドウモードに設定
+	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);	//指定の数値でウィンドウを表示する
+	SetWindowStyleMode(GAME_WINDOW_BAR);	//タイトルバーはデフォルトにする
+	SetMainWindowText(TEXT(GAME_WINDOW_NAME));	//ウィンドウのタイトルの文字
+}
